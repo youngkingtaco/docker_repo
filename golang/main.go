7b2bd0f89c21bdd0e1de9099bed9f83b53a9d1f9
Rodri Calderone <rodri.ocalderone97@icloud.com>
2020-09-03 14:01:10 +0000

    testing the Dockerfile by providing the full path in CMD option

--- golang/main.go
+++ golang/main.go
@@ -12,5 +12,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServer("0.0.0.0:8080", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
