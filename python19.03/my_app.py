a8297e69ef09d19afefcbef74751b8c1c8f2c802
Rodri Calderone <rodri.ocalderone97@icloud.com>
2020-09-05 15:41:11 +0000

    I have fixed the code from my_app.py

--- python19.03/my_app.py
+++ python19.03/my_app.py
@@ -1,9 +1,9 @@
-import http.server 
-import socketserver 
+import SimpleHTTPServer
+import SocketServer 
 
 PORT = 8080 
-Handler = http.server.SimpleHTTPRequestHandler
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT, Handler)) as httpd:
-  print("serving at port", PORT )
-  httpd.serve_forever()
+httpd = SocketServer.TCPServer(("",PORT), Handler)
+print ("serving at port", PORT)
+httpd.serve_forever()
